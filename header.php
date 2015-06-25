c13dc278e1a9535ab52ca4d4ff987dcc3eeebd46
juanobrach <juanobrach@gmail.com>
2015-06-25 19:30:58 -0300

    adding common wp files

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <title>
+            <?php wp_title('&#124;', true, 'right'); ?>
+        </title> 
+</head>
+<body>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying front page pages.
+ *
+ */
+?>
+<?php get_header(); ?> 
+<h1>Hola</h1>
+
+<?php get_footer() ?>
